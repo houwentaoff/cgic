967375eccfd81735a11b581844820871e835a5f2
housir <housir@housir-virtual-machine.(none)>
2014-08-26 20:26:21 +0800

    [FEA] add javascript,css.

---
+++ include.h
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *       Copyright (c), 2013-2020, Goke.
+ *       Filename:  include.h
+ *
+ *    Description:  
+ *         Others:
+ *   
+ *        Version:  1.0
+ *        Date:  2014/8/26 19:39:42
+ *       Revision:  none
+ *       Compiler:  xxx-gcc
+ *
+ *         Author:  Sean Hou , houwentaoff@gmail.com
+ *   Organization:  Goke
+ *        History:   Created by housir
+ *
+ * =====================================================================================
+ */
+
+#ifndef __INCLUDE_H__
+#define __INCLUDE_H__
+
+#include "err.h"
+#include "cgic.h"
+#include "page.h"
+#include "params.h"
+
+#endif//__include_h__
---
+++ page.h
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *       Copyright (c), 2013-2020, Goke.
+ *       Filename:  page.h
+ *
+ *    Description:  
+ *         Others:
+ *   
+ *        Version:  1.0
+ *        Date:  2014/8/26 19:34:15
+ *       Revision:  none
+ *       Compiler:  xxx-gcc
+ *
+ *         Author:  Sean Hou , houwentaoff@gmail.com
+ *   Organization:  Goke
+ *        History:   Created by housir
+ *
+ * =====================================================================================
+ */
+#ifndef  __PAGE_H__
+#define  __PAGE_H__
+
+#include "include.h"
+
+int   view_page();
+int   enc_page();
+int   pm_page();
+int   osd_page();
+int   sys_page();
+
+
+#endif//__PAGE_H__
+
